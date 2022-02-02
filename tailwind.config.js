44a82980abb39b405bb592064e1823912e79e87a
Sarasij Windows <sarasij@thepalefire.com>
2022-02-02 21:42:13 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./src/**/*.{html,js}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
